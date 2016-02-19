6482cdab10b4dae1e8ab5e5c17331053e5154a46
atulkumarr123 <atulkumarr123@gmail.com>
2016-02-19 11:34:45 +0530

    some more wrap up

---
+++ resources/views/commons/test.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-inverse" role="navigation">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+		</div>
+		<div class="collapse navbar-collapse" id="navbar-collapse-1">
+			<ul class="nav navbar-nav">
+				<li><a class="active"href="#">Home</a></li>
+				<li><a href="#">Courses</a></li>
+				<li><a href="#">About Us</a></li>
+				<li><a href="#">Videos</a></li>
+			</ul>
+			<div class="navbar-right">
+				<form class="navbar-form" role="search">
+					<div class="form-group">
+						<input type="text" class="form-control" placeholder="Search">
+					</div>
+					<button type="submit" class="btn btn-default">Go</button>
+				</form>
+			</div>
+		</div>
+</nav>
